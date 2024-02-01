a9fd15f5ebeb88b57cf295f7968caefdc091d445
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-01 16:42:45 +0500

    use of namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/posts', to: 'posts#index', as: 'posts'
-
-  get '/posts/new', to: 'posts#new', as: 'new_post'
-
-  post '/posts', to: 'posts#create'
-
-  get '/posts/:id', to: 'posts#show', as: 'post'
-
-  get '/posts/:id/edit', to: 'posts#edit', as: 'edit_post'
-
-  patch '/posts/:id', to: 'posts#update'
-
-  delete '/posts/:id', to: 'posts#destroy'
-
-  root 'posts#index'
+  namespace :admin do
+    resources :posts
+  end
+root 'posts#index'
 end
