d1fa712042ec85201f3c3b3c4bd0c1213eebfc8b
mmohhsin <m.mohsin@xcorebit.com>
2024-02-06 18:48:16 +0500

    validates_each validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user, inverse_of: :posts
+  validates_each :name, :body do |record, attr, value|
+    record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower:]]/
+  end
 end
