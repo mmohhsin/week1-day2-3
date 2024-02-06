a1c699ca63ff30d31b21d89458c0e27e76c103c4
mmohhsin <m.mohsin@xcorebit.com>
2024-02-06 17:00:29 +0500

    presence validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,3 @@
 class Post < ApplicationRecord
-  after_commit :notify_subscribers, on: [:create, :update]
-  after_rollback :log_failed_creation, on: [:create]
-
-  private
-
-  def notify_subscribers
-    puts "Notification sent to user on console!"
-  end
-
-  def log_failed_creation
-    puts "Failed to create post. Rolling back changes..."
-  end
+  validates :name, :body, presence: true
 end
