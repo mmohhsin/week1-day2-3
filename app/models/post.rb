b2824180f64fba5c130dfe4a7a05f6023da9e84e
mmohhsin <m.mohsin@xcorebit.com>
2024-02-06 18:19:03 +0500

    exclusion

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,3 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  validates :name, :body, presence: true
-  validates :name, uniqueness: true
-  validates :name, length: {minimum:2, maximum:3}
+  validates :name, exclusion: { in: %w(mobile laptop), message: "%{value} is not allowed." }
 end
