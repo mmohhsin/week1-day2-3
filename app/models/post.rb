944826fea7c1cedb26c01b394ae0c3c3679fdde8
mmohhsin <m.mohsin@xcorebit.com>
2024-02-06 14:54:32 +0500

    Before validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  before_validation :capitalize_name
+
+  validates :name, presence: true
+  validates :body, presence: true
+
+  private
+
+  def capitalize_name
+    self.name = name.capitalize if name.present?
+  end
 end
